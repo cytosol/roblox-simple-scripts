1330bf1bb7b9bbf1023fd036b4a81d408b42ed0b
Nicholas <nicksteinmetz2@gmail.com>
2016-02-26 09:00:46 -0500

    Update banscript.lua

--- banscript.lua
+++ banscript.lua
@@ -1,1 +1,11 @@
-
+-- Horsie!
+local banned = {["Banned"] = true} -- Banned peeps
+local message = "" -- Add a ban message here!
+function CheckBan(plr)
+    if banned[plr.Name] then
+       plr:Kick(message)
+    end
+end
+game.Players.ChildAdded:connect(function(new)
+   CheckBan(new)
+end
