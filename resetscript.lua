f35dee7ccc46011fae0f3e6bf20e196b69016177
Nicholas <nicksteinmetz2@gmail.com>
2016-02-26 11:41:32 -0500

    Update resetscript.lua

--- resetscript.lua
+++ resetscript.lua
@@ -1,6 +1,8 @@
 -- Made by Horsie!
 game.Players.PlayerAdded:connect(function(p)
-  p.Chatted:connect(function(msg)
-    
+  p.Chatted:connect(function(msg, plr)
+    if msg == "reset" and plr.Character then
+      plr.Character.Humanoid.Health = 0
+    end
   end)
 end)
