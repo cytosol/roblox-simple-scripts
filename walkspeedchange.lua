5d07c6bc6dfc164841c3d06a0722627cff489856
Nicholas <nicksteinmetz2@gmail.com>
2016-02-26 11:37:12 -0500

    Update walkspeedchange.lua

--- walkspeedchange.lua
+++ walkspeedchange.lua
@@ -1,1 +1,8 @@
-
+-- Made by Horsie!
+local ws = 16 -- Enter the walkspeed you want
+game.Players.PlayerAdded:connect(function(new)
+  local char = new.Character
+  if new and char then
+    char.Humanoid.Walkspeed = ws
+  end
+end
