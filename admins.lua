5e190dc18b18837c956f938836b550b55b6ab7a8
Nicholas <nicksteinmetz2@gmail.com>
2016-02-26 09:05:16 -0500

    Update admins.lua

--- admins.lua
+++ admins.lua
@@ -1,1 +1,18 @@
-
+-- Made by Horsie!
+local Admins = {}
+-----------------
+function tableContains(t, value)
+    for _, v in pairs(t) do
+        if v == value then 
+            return true
+        end
+    end
+    return false
+end
+function findPlayer(name)
+    for _, player in pairs(game.Players:GetPlayers()) do
+        if player.Name:lower() == name:lower() then
+            return player
+        end
+    end
+end
